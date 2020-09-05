98aaa1901eaea1bd42cb380b7c7b7df6009c63e3
Josh Corino <joshuc98@gmail.com>
2020-09-05 16:07:50 -0300

    Execution script added

---
+++ ExecutionScript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Q5="SET GLOBAL validate_password_policy=LOW;"
+Q1="CREATE DATABASE IF NOT EXISTS db_todo;"
+Q2="create user 'springuser'@'localhost' identified by 'ThePassword';"
+Q3="GRANT ALL ON *.* TO 'springuser'@'localhost' IDENTIFIED BY 'ThePassword';"
+Q4="FLUSH PRIVILEGES;"
+SQL="${Q5}${Q1}${Q2}${Q3}${Q4}"
+ 
+
+sudo mysql -h localhost -u root -p -e "$SQL"
+
+cd server
+mvn spring-boot::run &
+cd ..
+cd client
+sudo npm install
+ng serve &
